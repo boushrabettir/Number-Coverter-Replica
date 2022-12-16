8b38675967cede05e4d0e44ed8d19da1b70434af
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2022-12-15 16:19:37 -0800

    Add files via upload

---
+++ numberconverter/README.md
@@ -0,0 +1,4 @@
+# NumberConverter
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/boushrabettir/pen/ExRqOVB](https://codepen.io/boushrabettir/pen/ExRqOVB).
+
---
+++ numberconverter/dist/script.js
@@ -0,0 +1,19 @@
+const feet = 1/12;
+const meters = 100;
+
+const numberOfDayInput = document.querySelector('#number-input');
+const calculatorButton = document.querySelector('#calculate-button');
+const theFeet = document.querySelector('#feet');
+const theMeters = document.querySelector('#meters');
+
+
+calculatorButton.addEventListener('click',() => {
+  let number = numberOfDayInput.value;
+  let calculationFeet = number * feet;
+  let calculationMeters = number / meters;
+ 
+  
+  theFeet.innerHTML =`${calculationFeet} feet`;
+  theMeters.innerHTML = `${calculationMeters} meters`;
+
+});
